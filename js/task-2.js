cc966bae3afc6d7d698430126a0ce4f0cd2959db
juliasobocinska <juliasobocinska16.02@wp.pl>
2024-08-07 19:13:09 +0200

    upp

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage{
+    #items;
+
+    constructor(params) {
+        this.#items = params;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+       const indexItem = this.#items.indexOf(itemToRemove);
+       if(indexItem !== -1) {
+        this.#items.splice(indexItem, 1);
+       }
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
